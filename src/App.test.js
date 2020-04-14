c5743ed8c68be56e831b8302996b7f2f264f336d
Jimi W <jimi.welling@kuuasema.com>
2020-04-14 16:35:00 +0300

    Test app renders all posts

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    const {getByText} = render(<App/>);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+it('renders all posts', () => {
+    const posts = [{header: 'first'},
+        {header: 'second'}];
+    const {getByText} = render(<App posts={posts}/>);
+
+    posts.forEach(post => expect(getByText(post.header, {exact: false})).toBeInTheDocument());
 });
